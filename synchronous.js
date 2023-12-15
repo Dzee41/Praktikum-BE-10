5792183d76b32d903952570715d58e091637d89b
Dzee41 <zei.dhe21@gmail.com>
2023-12-15 10:06:21 +0700

    praktikum BE-10

---
+++ synchronous.js
@@ -0,0 +1,9 @@
+console.log("Zaid Membuka Bowser Google Chrome.");
+
+console.log(`
+Zaid download file selama 1 jam.
+  Downloading 1 Hour ...
+  Download Complete.
+`);
+
+console.log("Zaid membuka Youtube.");
